ac3818cdc7850b0e16c2a4e788fbb4b1810ee80f
Ten <z85146549@163.com>
2021-04-14 15:15:57 +0800

    feat(config): add config for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'mobile-official',
+      exec_mode: 'fork',
+      instances: '1', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start'
+    }
+  ]
+}
